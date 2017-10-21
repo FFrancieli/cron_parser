d5b57c19d75f7078c456b0898b8ceb2676c2b1de
Francieli Viane <francielifrv@gmail.com>
2017-10-21 22:20:21 +0200

    Parses minutes string to minute list

---
+++ src/main/java/cronExpression/CronExpression.java
@@ -0,0 +1,60 @@
+package cronExpression;
+
+import range.Range;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CronExpression {
+    private final int MAXIMUM_MINUTES = 59;
+
+    private final List<Integer> minute;
+
+    public CronExpression(String minute) {
+        this.minute = parseToMinutesList(minute);
+    }
+
+    private List<Integer> parseToMinutesList(String minute) {
+        if (minute.contains(",")) {
+            return splitIntervalIntoIntegerList(minute, ",");
+        }
+        if (minute.contains("-")) {
+            List<Integer> rangeBoundaries = splitIntervalIntoIntegerList(minute,"-");
+
+            int startOfRange = rangeBoundaries.get(0);
+            int endOfRange = rangeBoundaries.get(1);
+
+            return Range.range(startOfRange, endOfRange);
+        }
+        if(minute.contains("*/")) {
+            int step = Integer.parseInt(minute.split("/")[1]);
+
+            return Range.range(0, 59, step);
+        }
+        if (minute.contains("/")) {
+            List<Integer> minutesInterval = splitIntervalIntoIntegerList(minute, "/");
+
+            Integer rangeStart = minutesInterval.get(0);
+            Integer rangeStep = minutesInterval.get(1);
+
+           return Range.range(rangeStart, MAXIMUM_MINUTES, rangeStep);
+        }
+        if (minute.equals("*")) {
+            return Range.range(0, MAXIMUM_MINUTES);
+        }
+
+
+        return Collections.singletonList(Integer.parseInt(minute));
+    }
+
+    private List<Integer> splitIntervalIntoIntegerList(String minute, String delimiter) {
+        return Arrays.stream(minute.split(delimiter))
+                .map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    public List<Integer> getMinute() {
+        return minute;
+    }
+}
---
+++ src/test/java/cronExpression/CronExpressionTest.java
@@ -0,0 +1,71 @@
+package cronExpression;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CronExpressionTest {
+
+    @Test
+    public void returnsListWithMinutesWhenMinutesFieldIsNumericOnCronExpression() throws Exception {
+        CronExpression cronExpression = new CronExpression("20");
+
+        List<Integer> minutes = cronExpression.getMinute();
+        assertThat(minutes.size(), is(1));
+        assertThat(minutes, hasItem(20));
+    }
+
+    @Test
+    public void returnsListWithEveryNumberSeparatedByComaOnMinutesField() throws Exception {
+        CronExpression cronExpression = new CronExpression("1,5");
+
+        List<Integer> minutes = cronExpression.getMinute();
+        assertThat(minutes.size(), is(2));
+        assertThat(minutes, hasItems(1, 5));
+    }
+
+    @Test
+    public void returnsListWithRangeOfMinutesWhenMinuteFieldContainsDash() throws Exception {
+        CronExpression cronExpression = new CronExpression("1-5");
+
+        List<Integer> minutes = cronExpression.getMinute();
+        assertThat(minutes.size(), is(5));
+        assertThat(minutes, hasItems(1, 2, 3, 4, 5));
+    }
+
+    @Test
+    public void parsesMinutesToListAtEveryFifteenMinutesExpressionStartingFromOne() throws Exception {
+        CronExpression cronExpression = new CronExpression("1/15");
+
+        List<Integer> minute = cronExpression.getMinute();
+        assertThat(minute.size(), is(4));
+        assertThat(minute, hasItems(1, 16, 31, 46));
+    }
+
+    @Test
+    public void parsesMinutesToListAtEveryFifteenMinutesExpression() throws Exception {
+        CronExpression cronExpression = new CronExpression("*/15");
+
+        List<Integer> minute = cronExpression.getMinute();
+        assertThat(minute.size(), is(4));
+        assertThat(minute, hasItems(0, 15, 30, 45));
+    }
+
+    @Test
+    public void returnsListWithAllNumberFrom0To59ForCronExpressionWithAsteriskOnly() throws Exception {
+        CronExpression cronExpression = new CronExpression("*");
+
+        List<Integer> minutes = cronExpression.getMinute();
+        List<Integer> everyMinute = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+                20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+                46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59);
+
+        assertThat(minutes, is(everyMinute));
+    }
+}
